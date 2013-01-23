4f605ef72006728c052a74a6cd4c1ae6b8250715
antidote.crk <antidote.crk@gmail.com>
2013-01-23 05:12:07 +0000

    * Fixed line endings

--- save editor/svn_template/makesvnrev.sh
+++ save editor/svn_template/makesvnrev.sh
@@ -1,6 +1,6 @@
-REV=`svnversion -n ../`
+REV=`svnversion -n ../ | sed -e 's/.*://' -e 's/\([0-9]*\).*/\1/' | grep '[0-9]'`
 echo $REV
-cat > ./svnrev.h <<EOF
+cat > ./svnrev.h << EOF
 #define SVN_REV $REV
 #define SVN_REV_STR "$REV"
 EOF
